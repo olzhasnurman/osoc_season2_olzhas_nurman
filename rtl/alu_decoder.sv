cd85b73d3062410cd0015ac1be456875d89dac31
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-09-29 15:59:49 +0500

    Fix: Removed illegal instruction flag, csr instruction decoder logic.

--- rtl/alu_decoder.sv
+++ rtl/alu_decoder.sv
@@ -2,7 +2,7 @@
 
 // -----------------------------------------------------------------------
 // ALU decoder is a module designed to output alu control signal based on
-// op[5], alu_op, func_3, func_7[5] signals. 
+// op[5], alu_op, func3, func7[5] signals. 
 // -----------------------------------------------------------------------
 
 module alu_decoder 
@@ -10,32 +10,30 @@ module alu_decoder
 (
     // Input interface.
     input  logic [ 2:0 ] i_alu_op,
-    input  logic [ 2:0 ] i_func_3,
-    input  logic         i_func_7_5,
+    input  logic [ 2:0 ] i_func3,
+    input  logic         i_func7_5,
     input  logic         i_op_5,
 
     // Output interface. 
-    output logic [ 4:0 ] o_alu_control,
-    output logic         o_illegal_instr
+    output logic [ 4:0 ] o_alu_control
 );
 
-    logic [ 1:0 ] s_op_func_7;
+    logic [ 1:0 ] s_op_func7;
 
-    assign s_op_func_7 = { i_op_5, i_func_7_5 };
+    assign s_op_func7 = { i_op_5, i_func7_5 };
 
     // ALU decoder logic.
     always_comb begin 
-        o_illegal_instr = 1'b0;
-
+        o_alu_control = '0;
         case ( i_alu_op )
             3'b000: o_alu_control = 5'b00000; // ADD for I type instruction: lw, sw.
-            3'b001: o_alu_control = 5'b00001; // SUB  for B type instructions: beq, bne.
+            3'b001: o_alu_control = 5'b00001; // SUB for B type instructions: beq, bne.
 
             // I & R Type.
             3'b010: 
-                case (i_func_3)
-                    3'b000: if ( s_op_func_7 == 2'b11 ) o_alu_control = 5'b00001; // sub instruciton.
-                            else                        o_alu_control = 5'b00000; // add & addi instruciton.
+                case (i_func3)
+                    3'b000: if ( s_op_func7 == 2'b11 ) o_alu_control = 5'b00001; // sub instruciton.
+                            else                       o_alu_control = 5'b00000; // add & addi instruciton.
 
                     3'b001: o_alu_control = 5'b00101; // sll & slli instructions.
 
@@ -46,7 +44,7 @@ module alu_decoder
                     3'b100: o_alu_control = 5'b00100; // xor instruction.
 
                     3'b101: 
-                        case ( i_func_7_5 )
+                        case ( i_func7_5 )
                             1'b0:   o_alu_control = 5'b01000; // srl & srli instructions.
                             1'b1:   o_alu_control = 5'b01001; // sra & srai instructions. 
                             default: o_alu_control = '0; 
@@ -61,37 +59,34 @@ module alu_decoder
 
             // I & R Type W.
             3'b011: 
-                case ( i_func_3 )
+                case ( i_func3 )
                     3'b000: 
-                        case ( s_op_func_7 )
+                        case ( s_op_func7 )
                             2'b11:   o_alu_control = 5'b01011; // SUBW.
                             2'b10:   o_alu_control = 5'b01010; // ADDW.
                             default: o_alu_control = 5'b01111; // ADDIW.
                         endcase
                     3'b001: o_alu_control = 5'b01100; // SLLIW or SLLW
-                    3'b101: if ( i_func_7_5 ) o_alu_control = 5'b01110; // SRAIW or SRAW.
-                            else              o_alu_control = 5'b01101; // SRLIW or SRLW. 
+                    3'b101: if ( i_func7_5 ) o_alu_control = 5'b01110; // SRAIW or SRAW.
+                            else             o_alu_control = 5'b01101; // SRLIW or SRLW. 
                     default: begin
-                        o_alu_control   = 5'b00000;
-                        o_illegal_instr = 1'b1;                        
+                        o_alu_control   = 5'b00000;                      
                     end
                 endcase 
 
             // CSR.
             // 3'b100: 
-            //     case ( i_func_3[1:0] ) 
+            //     case ( i_func3[1:0] ) 
             //         2'b01: o_alu_control = 5'b10000;
             //         2'b10: o_alu_control = 5'b10001;
             //         2'b11: o_alu_control = 5'b10010;
             //         default: begin
             //             o_alu_control   = '0;
-            //             o_illegal_instr = 1'b1; 
             //         end
             //     endcase
             
             default: begin
                 o_alu_control   = '0;
-                o_illegal_instr = 1'b1;
             end
 
         endcase
