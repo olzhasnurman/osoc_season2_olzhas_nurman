ceb4a9b25a7d1001ca5b6db6990488c9e80fa0c8
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-09-30 09:57:41 +0500

    Feat: Added logic for main decoder.

--- rtl/main_decoder.sv
+++ rtl/main_decoder.sv
@@ -11,8 +11,8 @@ module main_decoder
 
     // Output interface.
     output logic [ 2:0 ] o_imm_src,
-    output logic [ 1:0 ] o_result_src,
-    output logic [ 1:0 ] o_alu_op,
+    output logic [ 2:0 ] o_result_src,
+    output logic [ 2:0 ] o_alu_op,
     output logic         o_mem_we,
     output logic         o_reg_we,
     output logic         o_alu_src,
@@ -25,7 +25,7 @@ module main_decoder
         I_Type      = 4'b0000,
         I_Type_ALU  = 4'b0001,
         I_Type_JALR = 4'b0010,
-        I_Type_IW   = 4'b0011,
+        I_Type_ALUW = 4'b0011,
         S_Type      = 4'b0100,
         R_Type      = 4'b0101,
         R_Type_W    = 4'b0110,
@@ -36,90 +36,101 @@ module main_decoder
     } t_instruction;
 
     // Instruction decoder signal. 
-    t_instruction s_instr;
-    logic [ 2:0 ] s_instr_type;
+    t_instruction s_instr_type;
 
     //----------------------------
     // Instruction decoder logic.
     //---------------------- -----
     always_comb begin
         case ( i_op )
-            7'b0000011: s_instr = I_Type;
-            7'b0010011: s_instr = I_Type_ALU;
-            7'b1100111: s_instr = I_Type_JALR;
-            7'b0011011: s_instr = I_Type_IW;
-            7'b0100011: s_instr = S_Type;
-            7'b0110011: s_instr = R_Type;
-            7'b0111011: s_instr = R_Type_W;
-            7'b1100011: s_instr = B_Type;
-            7'b1101111: s_instr = J_Type;
-            7'b0010111: s_instr = U_Type_ALU;
-            7'b0110111: s_instr = U_Type_LOAD;
-            default   : s_instr = I_Type; // For now default to I type later for Illegal instruction. 
+            7'b0000011: s_instr_type = I_Type;
+            7'b0010011: s_instr_type = I_Type_ALU;
+            7'b1100111: s_instr_type = I_Type_JALR;
+            7'b0011011: s_instr_type = I_Type_ALUW;
+            7'b0100011: s_instr_type = S_Type;
+            7'b0110011: s_instr_type = R_Type;
+            7'b0111011: s_instr_type = R_Type_W;
+            7'b1100011: s_instr_type = B_Type;
+            7'b1101111: s_instr_type = J_Type;
+            7'b0010111: s_instr_type = U_Type_ALU;
+            7'b0110111: s_instr_type = U_Type_LOAD;
+            default   : s_instr_type = I_Type; // For now default to I type later for Illegal instruction. 
         endcase
     end
 
     instr_decoder INSTR_DEC (
-        .i_instr   ( s_instr      ),
-        .o_imm_src ( s_instr_type )
+        .i_instr   ( s_instr_type ),
+        .o_imm_src ( o_imm_src    )
     );
 
-    assign o_imm_src = s_instr_type;
-
-    //Decoder logic.
-    /*
-    ____________________________________
-    | control signal | instuction type |
-    |________________|_________________|
-    | 000            | I type          |
-    | 001            | S type          |
-    | 010            | B type          |
-    | 011            | J type          |
-    | 100            | U type          |
-    | 101            | R type          |
-    |__________________________________|
-    */
-
 
     //----------------------------------------------
-    // Result MUX control signal logic.
+    // Decoder for output control signals.
     //----------------------------------------------
     always_comb begin
-        o_result_src = 3'b0;
-        o_alu_op     = 2'b0;
+        // Default values.
+        o_result_src = 3'b0; // 000 - ALUResult, 001 - ReadDataMem, 010 - PCPlus4, 011 - PCPlusImm, 100 - ImmExtended.
+        o_alu_op     = 3'b0; // 000 - Add, 001 - Sub, 010 - I & R, I & R W.
         o_mem_we     = 1'b0;
         o_reg_we     = 1'b0;
-        o_alu_src    = 1'b0;
+        o_alu_src    = 1'b0; // 0 - Reg, 1 - Immediate.
         o_branch     = 1'b0;
-        o_jump       = 1'b0; 
+        o_jump       = 1'b0;
 
-        case ( s_instr )
-            I_Type,     : 
-            default: 
-        endcase
-
-        case ( s_instr_type ) 
-            3'b000: begin
-                o_reg_we = 1'b1;
+        case ( s_instr_type )
+            I_Type: begin
+                o_reg_we     = 1'b1;
+                o_alu_src    = 1'b1;
+                o_result_src = 3'b1;
             end
-            3'b001: begin
-                o_mem_we = 1'b1; // S-type
+            I_Type_ALU: begin
+                o_reg_we     = 1'b1;
+                o_alu_src    = 1'b1;
+                o_alu_op     = 3'b10;
             end
-            3'b010: begin
-                
+            I_Type_JALR: begin
+                o_reg_we     = 1'b1;
+                o_alu_src    = 1'b1; 
+                o_jump       = 1'b1;
+                o_result_src = 3'b10;
             end
-            3'b011: begin
-                o_reg_we = 1'b1;
+            I_Type_ALUW: begin
+                o_reg_we     = 1'b1;
+                o_alu_src    = 1'b1;
+                o_alu_op     = 3'b11; 
             end
-            3'b100: begin
-                o_reg_we = 1'b1;
+            S_Type: begin
+                o_mem_we     = 1'b1;
+                o_alu_src    = 1'b1;
             end
-            3'b101: begin
-                o_reg_we = 1'b1;
+            R_Type: begin
+                o_reg_we     = 1'b1;
+                o_alu_op     = 3'b10;
+            end
+            R_Type_W: begin
+                o_reg_we     = 1'b1;
+                o_alu_op     = 3'b11;
+            end
+            B_Type: begin
+                o_branch     = 1'b1;
+                o_alu_op     = 3'b1;
+            end
+            J_Type: begin
+                o_reg_we     = 1'b1;
+                o_jump       = 1'b1;
+                o_result_src = 3'b10;
+            end
+            U_Type_ALU: begin
+                o_reg_we     = 1'b1;
+                o_result_src = 3'b11;
+            end
+            U_Type_LOAD: begin
+                o_reg_we     = 1'b1; 
+                o_result_src = 3'b100; 
             end
             default: begin
                 o_result_src = 3'b0;
-                o_alu_op     = 2'b0;
+                o_alu_op     = 3'b0;
                 o_mem_we     = 1'b0;
                 o_reg_we     = 1'b0;
                 o_alu_src    = 1'b0;
