58a5761c07614ddaad97c9765883cbfd5c5ffe96
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-10-02 14:15:18 +0500

    Feat: Added pipeline register file for execute stage output signals.

---
+++ rtl/preg_execute.sv
@@ -0,0 +1,67 @@
+/* Copyright (c) 2024 Maveric NU. All rights reserved. */
+
+// ------------------------------------------------------------------------------------------
+// This is a nonarchitectural register file for execute stage pipelining.
+// ------------------------------------------------------------------------------------------
+
+module preg_execute
+// Parameters.
+#(
+    parameter DATA_WIDTH  = 64,
+              ADDR_WIDTH  = 64,
+              REG_ADDR_W  = 5
+)
+// Port decleration. 
+(   
+    //Input interface. 
+    input  logic                      i_clk,
+    input  logic                      i_arst,
+    input  logic [              2:0 ] i_result_src,
+    input  logic                      i_mem_we,
+    input  logic                      i_reg_we,
+    input  logic [ ADDR_WIDTH - 1:0 ] i_pc_plus4,
+    input  logic [ ADDR_WIDTH - 1:0 ] i_pc_target,
+    input  logic [ DATA_WIDTH - 1:0 ] i_imm_ext,
+    input  logic [ DATA_WIDTH - 1:0 ] i_alu_result,
+    input  logic [ DATA_WIDTH - 1:0 ] i_write_data,
+    input  logic [ REG_ADDR_W - 1:0 ] i_rd_addr,
+    
+    // Output interface.
+    output logic [              2:0 ] o_result_src,
+    output logic                      o_mem_we,
+    output logic                      o_reg_we,
+    output logic [ ADDR_WIDTH - 1:0 ] o_pc_plus4,
+    output logic [ ADDR_WIDTH - 1:0 ] o_pc_target,
+    output logic [ DATA_WIDTH - 1:0 ] o_imm_ext,
+    output logic [ DATA_WIDTH - 1:0 ] o_alu_result,
+    output logic [ DATA_WIDTH - 1:0 ] o_write_data,
+    output logic [ REG_ADDR_W - 1:0 ] o_rd_addr
+);
+
+    // Write logic.
+    always_ff @( posedge i_clk, posedge i_arst ) begin 
+        if ( i_arst ) begin
+            o_result_src  <= '0;
+            o_mem_we      <= '0;
+            o_reg_we      <= '0;
+            o_pc_plus4    <= '0;
+            o_pc_target   <= '0;
+            o_imm_ext     <= '0;
+            o_alu_result  <= '0;
+            o_write_data  <= '0;
+            o_rd_addr     <= '0;
+        end
+        else begin
+            o_result_src  <= i_result_src;
+            o_mem_we      <= i_mem_we;
+            o_reg_we      <= o_reg_we;
+            o_pc_plus4    <= i_pc_plus4;
+            o_pc_target   <= i_pc_target;
+            o_imm_ext     <= i_imm_ext;
+            o_alu_result  <= i_alu_result;
+            o_write_data  <= i_write_data;
+            o_rd_addr     <= i_rd_addr;
+        end
+    end
+    
+endmodule
