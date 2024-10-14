c5d3da56bee04c8d1b7c89317f8e3d9aa309c33e
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-10-14 12:18:27 +0500

    Feat: Modified i_mem simulation file.

--- rtl/i_mem.sv
+++ rtl/i_mem.sv
@@ -1,7 +1,7 @@
 /* Copyright (c) 2024 Maveric NU. All rights reserved. */
 
 // --------------------------------------------------------------------------------------
-// This is a instruction memory simulatrion file. Later to be replaced with cache.
+// This is a instruction memory simulation file.
 // --------------------------------------------------------------------------------------
 
 `define PATH_TO_MEM "/home/maveric/osoc_season2_olzhas_nurman/test/tests/add-riscv64-nemu.txt" // Later to be defined properly.
@@ -10,32 +10,44 @@ module i_mem
 // Parameters.
 #(
     parameter DATA_WIDTH = 32,
-              ADDR_WIDTH = 10,
-              MEM_DEPTH  = 256
+              ADDR_WIDTH = 64
 )
-// Port decleration. 
-(   
-    //Input interface. 
+(
+    // Input interface..
     input  logic                      i_clk,
     input  logic                      i_arst,
+    input  logic                      i_write_en,
+    input  logic [ DATA_WIDTH - 1:0 ] i_data,
     input  logic [ ADDR_WIDTH - 1:0 ] i_addr,
-    
-    // Output interface.
-    output logic [ DATA_WIDTH - 1:0 ] o_read_data
+
+    // Output signals.
+    output logic [ DATA_WIDTH - 1:0 ] o_read_data,
+    output logic                      o_successful_access,
+    output logic                      o_successful_read,
+    output logic                      o_successful_write
 );
+    logic [ DATA_WIDTH - 1:0 ] mem [ 524287:0];
 
-    // Memory block.
-    logic [ DATA_WIDTH - 1:0 ] mem_block [ MEM_DEPTH - 1:0 ];
 
-    // Write logic.
-    always_ff @( posedge i_clk, posedge i_arst ) begin 
-        if ( i_arst ) begin
-            $readmemh ( `PATH_TO_MEM, mem_block );
-        end
+    always_ff @( posedge i_clk, posedge i_arst ) begin
+        if      ( i_arst     ) $readmemh(`PATH_TO_MEM, mem);
+        else if ( i_write_en ) mem[ i_addr[ 20:2 ] ] <= i_data;
     end
 
-    // Read logic.
-    assign o_read_data = mem_block [ i_addr [ 9:2 ] ];
+
+    assign o_read_data         = mem[ i_addr [20:2] ];
+    assign o_successful_read   = 1'b1;
+    assign o_successful_write  = 1'b1;
+
+
+    // Simulating multiple clock cycle memory access.
+    logic [ 2:0 ] s_count;
+    always_ff @( posedge i_clk, posedge i_arst ) begin
+        if ( i_arst ) s_count <= '0;
+        else          s_count <= s_count + 3'b1;
+    end
+
+    assign o_successful_access = ( s_count == 3'b111 ); 
 
     
 endmodule
