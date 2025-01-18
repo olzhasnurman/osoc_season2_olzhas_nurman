6b205a17f660ae4a01e511345fd71a0ec0313288
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2025-01-18 22:21:26 +0500

    Test: Modified the the script to run the tests with different cache sizes.

--- run_tests.py
+++ run_tests.py
@@ -9,6 +9,8 @@ TEST_DIR = "./test/tests/list/list.txt"
 MEMORY_FILE = "./rtl/i_mem.sv"
 TB_FILE     = "./test/tb/tb_test_env.cpp"
 RESULT_FILE = "result.txt"
+TEST_ENV_FILE = "./rtl/test_env.sv"
+DCACHE_FILE   = "./rtl/dcache.sv"
 
 TEST_AM = []
 TEST_RV_ARCH= []
@@ -59,7 +61,7 @@ def clean_before():
         file_out.write("")
 
 
-def compile_single(test, gen_wave):
+def compile_single(test, gen_wave=False):
     modify_testbench(not gen_wave)
     modify_memory(TEST[test])
     os.system(COMPILE_C_COMMAND)
@@ -87,6 +89,79 @@ def compile_group(group):
              compile_single(test)
     else:
         print("Unrecognized test group")
+
+def compile_varying_cache():
+     block_size = 128
+     while block_size <= 1024:
+          set_count = 2
+          while set_count <= 16:
+               modify_cache_size(block_size, set_count)
+               os.system(SAVE_COMMAND)
+               with open (RESULT_FILE, 'r') as file_in:
+                    lines = file_in.readlines()
+           
+               old_lines = []
+               for line in lines:
+                   old_lines.append(line)
+           
+               with open(RESULT_FILE, 'w') as file_out:
+                    file_out.writelines(old_lines)
+                    message = "\n\nCACHE LINE WIDTH: " +  str(block_size) + " bits, SET_COUNT: " + str(set_count) + "\n"
+                    file_out.write(message)   
+
+               # compile_single("am-add", False)
+               compile_all()
+               set_count *= 2
+          block_size *= 2
+
+def modify_cache_size(block_size, set_count):
+    with open ( TEST_ENV_FILE, 'r' ) as file_in:
+        lines = file_in.readlines()
+
+    new_lines = []
+    parameter_found = False
+    for line in lines:
+        if 'parameter' in line:
+            parameter_found =True
+        
+        if parameter_found:
+            if 'BLOCK_WIDTH' in line:
+                 new_line = line[:31] + str(block_size)
+                 new_lines.append(new_line)
+                 new_lines.append("\n")
+                 parameter_found = False
+            else:
+                 new_lines.append(line)
+        else:
+             new_lines.append(line)
+
+    with open (TEST_ENV_FILE, 'w') as file_out:
+          file_out.writelines(new_lines)
+
+
+    with open ( DCACHE_FILE, 'r' ) as file_in:
+        lines = file_in.readlines()
+
+    new_lines = []
+    parameter_found = False
+    for line in lines:
+        if 'parameter' in line:
+            parameter_found =True
+        
+        if parameter_found:
+            if 'SET_COUNT' in line:
+                 new_line = line[:27] + str(set_count)
+                 new_lines.append(new_line)
+                 new_lines.append("\n")
+                 parameter_found = False
+            else:
+                 new_lines.append(line)
+        else:
+             new_lines.append(line)
+
+    with open (DCACHE_FILE, 'w') as file_out:
+          file_out.writelines(new_lines)
+
 
 def save_result(test):
     os.system(SAVE_COMMAND)
@@ -160,6 +235,12 @@ def modify_testbench(comment):
     with open (TB_FILE, 'w') as file_out:
           file_out.writelines(new_lines)
          
+def initial_note():
+    with open(RESULT_FILE, 'w') as file_out:
+        message_1 = "NOTE: ILLEGAL INSTRUCTION REFERS TO INSTRUCTIONS THAT WERE NOT (YET) IMPLEMENTED IN MAVERIC CORE 2.0 PROCESSOR. THE SYSTEM REGOGNIZES THOSE INSTRUCTIONS AS ILLEGAL.\n"
+        message_2 = "THE LIST INCLUDES, BUT NOT LIMITED TO, MUL, DIV, FENCE INSTRUCTIONS.\n"
+        file_out.write(message_1)
+        file_out.write(message_2)        
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -169,6 +250,7 @@ def parse_arguments():
     parser.add_argument('-g', '--compile-group', type=str)
     parser.add_argument('-c', '--clean', action='store_true', default=False)
     parser.add_argument('-t', '--trace', action='store_true', default=False)
+    parser.add_argument('-v', '--compile-varying-cache', action='store_true', default=False)
 
     return parser.parse_args()
 
@@ -176,6 +258,8 @@ def main():
     clean_before()
     args = parse_arguments()
 
+    initial_note()
+  
     if args.compile_single:
         compile_single(args.compile_single, args.trace)
     elif args.list_tests:
@@ -184,6 +268,8 @@ def main():
          compile_all()
     elif args.compile_group:
          compile_group(args.compile_group)
+    elif args.compile_varying_cache:
+         compile_varying_cache()
     elif args.clean:
          clean_after()
     else:
